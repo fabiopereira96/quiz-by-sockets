102075151fdee50d7d960ca92140b188818f602d
Fabio Alves Pereira <fabio@macbook-air-fabio.local>
2019-12-01 12:09:46 -0300

    Adicionando envio de mensagens info para os clients

--- client/client.py
+++ client/client.py
@@ -3,21 +3,23 @@ import os
 import subprocess
 
 s = socket.socket()
-host = '192.168.0.14'
+host = '192.168.1.2'
 port = 9999
 
-s.connect((host, port))
+try:
+    s.connect((host, port))
+    print("Conexão estabelecida com o servidor.")
 
-while True:
-    data = s.recv(1024)
-    if data[:2].decode("utf-8") == 'cd':
-        os.chdir(data[3:].decode("utf-8"))
+    while True:
+        data = s.recv(1024)
+        
+        #info
+        if data[:5].decode("utf-8") == '#info':
+            print(data[5:].decode("utf-8"))
+            s.send(str.encode("ok"))
+        
+        #question
 
-    if len(data) > 0:
-        cmd = subprocess.Popen(data[:].decode("utf-8"),shell=True, stdout=subprocess.PIPE, stdin=subprocess.PIPE, stderr=subprocess.PIPE)
-        output_byte = cmd.stdout.read() + cmd.stderr.read()
-        output_str = str(output_byte,"utf-8")
-        currentWD = os.getcwd() + "> "
-        s.send(str.encode(output_str + currentWD))
-
-        print(output_str)
+except socket.error as msg:
+    print("Não foi possível estabelecer uma conexão com o servidor na porta: " + 
+    str(port) + " Error: " + str(msg))
--- server/quiz_socket.py
+++ server/quiz_socket.py
@@ -18,7 +18,6 @@ class SocketQuiz():
     def init_connections(self):
         #Fecha todas as conexoes ja existentes antes de iniciar loop
         self.close_connections()
-
         while True:
             try:
                 #Aguarda nova conexao
@@ -31,6 +30,7 @@ class SocketQuiz():
 
                 print("\nNova conexao estabelecida com: " + addrs[0])
                 print("\nTemos um total de " + str(len(self.connections)) + " conexoes")
+                self.send_waiting_message(conn)
 
             except socket.error as msg:
                 print("Error ao aceitar novas conexoes" + str(msg)) 
@@ -64,6 +64,20 @@ class SocketQuiz():
             list_conn += str(i) + "   " + str(self.address[i][0]) + "   " + str(self.address[i][1]) + "\n"
         print("------------------------------------------------------" + "\n" 
             + "\t\t Conexoes existentes \t\t\n" + list_conn)
+
+    def send_waiting_message(self, conn):
+        length_conn = len(self.connections)
+        self.send_message(conn, "#info Aguardando um adversário para você. Em instantes o Quiz será iniciado!")
+        if length_conn > 1:
+            for i, conni in enumerate(self.connections):
+                self.send_message(conni, "#info Boa notícia, seu adiversário já está conectado e iremos iniciar o Quiz!")
+            
+    def send_message(self, conn, message):
+        try:
+            conn.send(str.encode(message))
+            conn.recv(20480)
+        except:
+            print("Não foi possível enviar a mensagem para a conexão: " + str(conn))
 
     def get_connected_client_by_id(self, id):
         try:
